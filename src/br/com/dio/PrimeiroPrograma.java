cbc656dcf1f698d0e5418b93354c2e45a2f14251
jfranquis <@Multcom1102>
2022-07-17 16:24:08 -0300

    - Class - Main

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,11 +1,18 @@
 package br.com.dio;
 
+import br.com.dio.model.Gato;
+
 public class PrimeiroPrograma {
 
     public static void main(String[] args) {
-        int a = 5;
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+/*        int a = 5;
         int b = 5;
-        System.out.println("Hello World! " + (a+b));
+        System.out.println("Hello World! " + (a+b));*/
 
     }
 }
