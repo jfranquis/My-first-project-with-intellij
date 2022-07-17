bb29289c40f9abb613ff507699583321f65eccab
jfranquis <@Multcom1102>
2022-07-17 16:07:27 -0300

    - Class - Main

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        System.out.println("Hello World! " + (a+b));
+
+    }
+}
